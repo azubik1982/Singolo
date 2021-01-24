5a13b529fc9734017d14508e98b3d0546a10ea94
Andrei Zubik <azubik1982@gmail.com>
2021-01-25 00:44:23 +0300

    refactor: find bug in portfolio menu block with bordering father-element

--- script.js
+++ script.js
@@ -7,14 +7,49 @@ MENU.addEventListener('click', (event) => {
     event.target.classList.add('active');
 });
 
+// // menu-navigation-scrolling function
+
+// jQuery(window).scroll(function(){
+//     var $sections = $('section');
+
+//     $sections.each(function(i,el){
+
+//     var top  = $(el).offset().top-100;
+//     var bottom = top +$(el).height();
+//     var scroll = $(window).scrollTop();
+//     var id = $(el).attr('id');
+
+//     if( scroll > top && scroll < bottom){
+//         $('a.active').removeClass('active');
+//     $('a[href="#'+id+'"]').addClass('active');
+
+//     }
+//     })
+//     });
+
+//     $("nav").on("click","a", function (event) {
+//     // исключаем стандартную реакцию браузера
+//     event.preventDefault();
+
+//     // получем идентификатор блока из атрибута href
+//     var id  = $(this).attr('href'),
+
+//     // находим высоту, на которой расположен блок
+//         top = $(id).offset().top;
+
+//     // анимируем переход к блоку, время: 800 мс
+//     $('body,html').animate({scrollTop: top}, 800);
+//     });
 
 // menu-portfolio
 
 PORTFOLIO.addEventListener('click', (event) => {
-  PORTFOLIO.querySelectorAll('button').forEach(el => el.classList.remove('button_borderd_active'));
-    event.target.classList.add('button_borderd_active');
+  if (event.target.classList.contains("button_borderd")) {
+      PORTFOLIO.querySelectorAll('button').forEach(el => el.classList.remove('button_borderd_active'));
+      if (event.target.classList.contains("button_borderd")) {
+          event.target.classList.add('button_borderd_active');}
+  }
 });
-
 
 // slider-skript
 
