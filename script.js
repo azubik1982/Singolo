ca3f04a20feccad378c349fa5af9a37663bc1686
azubik1982 <azubik1982@gmail.com>
2021-03-01 18:51:41 +0300

    refactor: change Slider-formating, start for 17 try

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const MENU = document.getElementById('MENU');
 // menu-navigation
 
 MENU.addEventListener('click', (event) => {
-    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    MENU.querySelectorAll('#MENU a').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
 
